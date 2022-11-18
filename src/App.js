4f33e69331903548f26018dfd0dc108b4104b27e
KevinBruce <kevinlawbruce@gmail.com>
2022-11-18 14:30:48 -0500

    created axios call for data

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import './App.css';
+import React, { useEffect, useState } from 'react';
 import Tweet from './Tweet';
+import axios from 'axios';
 
 function App() {
+
+  const [data, setData] = useState([]);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('https://coursera-twitter-api.herokuapp.com/tweets')
+      setData(response.data);
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  useEffect(() => {
+    fetchData()
+  })
+  
+  
   return (
     <div>
-      <Tweet
-      text='Text passed down'
-      author='dog'
-      date='18/11/2022' 
-      />
+      {data.map(tweet => <Tweet key={tweet.id} {...tweet}/>)}
     </div>
   );
 }
--- src/Tweet.js
+++ src/Tweet.js
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+
 
 const Tweet = ({text, date, author}) => {
-    // const text = props.text;
-    // const author = props.author;
-    // const date = props.date;
+
+    const [counter, setCounter] = useState(0);
+
+    const increment = () => {
+        setCounter( counter + 1 );
+    }
+
+    const decrease = () => {
+        setCounter( counter - 1 );
+    }
+    
     return (
         <div>
+            <h1>counter: {counter}</h1>
             <h3>{text}</h3>
             <p>{author}</p>
             <p>{date}</p>
+            <button onClick={increment}>+</button>
+            <button onClick={decrease}>-</button>
         </div>
     )
 };
